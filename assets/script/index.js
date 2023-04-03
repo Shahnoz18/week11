60989ad50b9762b1e3db084a4aab89e8bda70ca2
Shahnoz18 <shob181296@mail.ru>
2023-04-03 23:28:54 +0300

    end

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,47 +1,42 @@
 const cards = document.querySelectorAll(".card");
 
 const cardPrice1 = document.getElementById("card-price1");
+console.log(cardPrice1);
 const cardPrice2 = document.getElementById("card-price2");
 const cardPrice3 = document.getElementById("card-price3");
 const cardPrice4 = document.getElementById("card-price4");
+const showDiscount = document.querySelector(".container__discount");
 
 
-let sum = +cardPrice1.textContent + +cardPrice2.textContent + +cardPrice3.textContent + +cardPrice4.textContent;
-console.log(sum);
+const calcSum = () => {
+    return sum = +cardPrice1.textContent + +cardPrice2.textContent + +cardPrice3.textContent + +cardPrice4.textContent;
+};
+//console.log(calcSum());
+
 let result = document.getElementById("result");
-result.textContent = sum;
+result.textContent = calcSum();
 
 
-let showDiscount = document.querySelector(".container__discount");
-
-// function showPercent(number) {
-//     return number * 0.8;
-// }
-
-// По клику вывести в хтмл каждую цену * 0.8
-const discountTotal = showPercent => {
-	let showNewPrice = +showPercent.textContent * 0.8;
-	return showPercent.textContent = Math.round(showNewPrice);
+const showPercent = (number) => {
+    let newNumber = Math.round(number.textContent * 0.8);
+    console.log(number);
+    return number.textContent = newNumber;
 };
 
-const showResult = () => {
-	sum = +cardPrice1.textContent + +cardPrice2.textContent + +cardPrice3.textContent + +cardPrice4.textContent;
-	return result.textContent = sum;
-};
-
-function wrapperToDiscount() {
-	discountTotal(cardPrice1);
-	discountTotal(cardPrice2);
-	discountTotal(cardPrice3);
-	discountTotal(cardPrice4);
-	showResult();
+function calcDiscount() {
+    showPercent(cardPrice1);
+    //console.log(cardPrice1);
+    showPercent(cardPrice2);
+    showPercent(cardPrice3);
+    showPercent(cardPrice4);
+    result.textContent = calcSum();
 }
-showDiscount.addEventListener('click', wrapperToDiscount, {once: true});
 
-// showDiscount.addEventListener("click", () => {
-//     const onePay = showPercent(cardPrice1);
-//     console.log(onePay);
-//     cardPrice1.textContent = onePay;
+showDiscount.addEventListener("click", calcDiscount, {once: true}
+   
+
+    // console.log(onePay);
+    // cardPrice1.textContent = onePay;
     // const twoPay = showPercent(cardPrice2);
     // const threePay = showPercent(cardPrice3);
     // const fourPay = showPercent(cardPrice4); 
@@ -49,28 +44,4 @@ showDiscount.addEventListener('click', wrapperToDiscount, {once: true});
     // cardPrice2.textContent = twoPay;
     // cardPrice3.textContent = threePay;
     // cardPrice4.textContent = fourPay;
-// }
-
-// )
-
-// const showDiscount = showPercent => {
-//     let showNewPrice = +showPercent.textContent * 0.8;
-//     return showPercent.textContent = showNewPrice
-// }
-
-// //По клику вывести в хтмл каждую цену * 0.8
-// const discountTotal = showPrice => {
-// 	let showNewPrice = +showPrice.textContent * 0.8;
-// 	return showPrice.textContent = showNewPrice;
-// };
-
-// function wrapperToDiscount() {
-// 	discountTotal(priceItem1);
-// 	discountTotal(priceItem2);
-// 	discountTotal(priceItem3);
-// 	discountTotal(priceItem4);
-// 	discountTotal(priceItem5);
-// 	total.textContent = sumPriceTotal();
-// }
-
-// btn.addEventListener('click', wrapperToDiscount);
+);
